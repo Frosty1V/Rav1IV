94c02d7a28adea770ab6aacf2f6ca70619d78fa4
Frosty1V <147980724+Frosty1V@users.noreply.github.com>
2024-05-17 16:27:15 +0300

    Update and rename اضافة.js to اقتراح.js

--- plugins/اقتراح.js
+++ plugins/اقتراح.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `هذا الامر اذا كنت تمتلك فكرة تريد ان تضاف الى البوت`
+  if (!text) throw `هذا الامر اذا كنت تمتلك اقتراح تريد ان تضاف الى البوت`
   if (text.length < 3) throw `يجب ان تكون الرسالة فيها اكثر من اربع كلمات `
   if (text.length > 1000) throw `الحد الادنى هو 1000 حرف اختر ما تريد قوله !`
   let teks = `*${command.toUpperCase()}!*\n\nمن : *@${m.sender.split`@`[0]}*\n\nالرساله : ${text}\n`
@@ -12,5 +12,5 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 }
 handler.help = ['report']
 handler.tags = ['infobot']
-handler.command = /^(اضافة|اضافه)$/i
+handler.command = /^(اقتراح)$/i
 export default handler

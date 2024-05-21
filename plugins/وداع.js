e15dbb45bfda5b357fe0b64908db1d8fbcac10e2
Frosty1V <147980724+Frosty1V@users.noreply.github.com>
2024-05-21 13:02:55 +0300

    Update وداع.js

--- plugins/وداع.js
+++ plugins/وداع.js
@@ -1,13 +1,16 @@
-//import db from '../lib/database.js'
+const handler = async (m, {conn, text, isROwner, isOwner}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.gc_setbye
 
-let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
-    global.db.data.chats[m.chat].sBye = text
-    m.reply('*الوداع!*')
-  } else throw `*احسن مكوناش طايقينك !*\n*أكــتب @user تم خروج!*`
-}
-handler.help = ['setbye <text>']
-handler.tags = ['group']
+    global.db.data.chats[m.chat].sBye = text;
+    m.reply(tradutor.texto1);
+  } else throw `${tradutor.texto2}\n*احسن مكوناش طايقينك !*\n*أكــتب @user تم خروج!*${tradutor.texto3}`
+};
+handler.help = ['setbye <text>'];
+handler.tags = ['group'];
 handler.command = ['الوداع'] 
 handler.admin = true
 

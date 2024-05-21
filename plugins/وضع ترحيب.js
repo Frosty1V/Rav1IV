6c53e79c2eb4b3222cba280c50ae5ea13b1681dc
Frosty1V <147980724+Frosty1V@users.noreply.github.com>
2024-05-21 13:00:35 +0300

    Update وضع ترحيب.js

--- plugins/وضع ترحيب.js
+++ plugins/وضع ترحيب.js
@@ -1,14 +1,17 @@
-//import db from '../lib/database.js'
 
-let handler = async (m, { conn, text, isROwner, isOwner }) => {
+const handler = async (m, {conn, text, isROwner, isOwner}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.gc_setwelcome
+
   if (text) {
-    global.db.data.chats[m.chat].sWelcome = text
-    m.reply('*تــم وضــع رســالــة الــتـرحـيب !*')
-  } else throw `*أدخــل رســالــة الـتـرحــيب !*\n*عــشان تــعـمل مـنــشن أكــتب @user, عــشان تـحط أســم الـجروب أكـتب @group, عــشان تـحط وصــف أكــتب @desc*`
-}
-handler.help = ['setwelcome <text>']
-handler.tags = ['group']
-handler.command = ['الترحيب'] 
-handler.admin = true
-
-export default handler
+    global.db.data.chats[m.chat].sWelcome = text;
+    m.reply(tradutor.texto1);
+  } else throw `${tradutor.texto2[0]}\n*أدخــل رســالــة الـتـرحــيب !*\n*عــشان تــعـمل مـنــشن أكــتب @user, عــشان تـحط أســم الـجروب أكـتب @group, عــشان تـحط وصــف أكــتب @desc*`;
+};
+handler.help = ['setwelcome <text>'];
+handler.tags = ['group'];
+handler.command = ['ترحيب'];
+handler.admin = true;
+export default handler;

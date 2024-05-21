ac9046c159fbf858d5efdbb65ffea8af89c982da
Frosty1V <147980724+Frosty1V@users.noreply.github.com>
2024-05-21 16:02:00 +0300

    Update البوت سوف يحسن الصوره.js

--- plugins/البوت سوف يحسن الصوره.js
+++ plugins/البوت سوف يحسن الصوره.js
@@ -2,133 +2,82 @@ import FormData from "form-data";
 import Jimp from "jimp";
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-	switch (command) {
-		case "dehaze":
-			{
-				conn.enhancer = conn.enhancer ? conn.enhancer : {};
-				let q = m.quoted ? m.quoted : m;
-				let mime = (q.msg || q).mimetype || q.mediaType || "";
-				if (!mime)
-					throw `ارسل للبوت اي صورة وسوف يقوم  بإضافة لمسة يجعلها جميلة وجذابة\n\nارسل الصورة تم أشر اليها واكتب \n*.نحسين*`;
-				if (!/image\/(jpe?g|png)/.test(mime))
-					throw `Mime ${mime} not support`;
-				else conn.enhancer[m.sender] = true;
-				m.reply(wait);
-				let img = await q.download?.();
-				let error;
-				try {
-					const This = await processing(img, "dehaze");
-					conn.sendFile(m.chat, This, "", "تم تحسين الصوره", m);
-				} catch (er) {
-					error = true;
-				} finally {
-					if (error) {
-						m.reply("*فشلت العملية*");
-					}
-					delete conn.enhancer[m.sender];
-				}
-			}
-			break;
-		case "recolor":
-			{
-				conn.recolor = conn.recolor ? conn.recolor : {};
-				let q = m.quoted ? m.quoted : m;
-				let mime = (q.msg || q).mimetype || q.mediaType || "";
-				if (!mime)
-					throw `ارسل للبوت اي صورة وسوف يقوم بالتلاعب وتغيير بعض الالوان في الصورة\n\nارسل الصورة تم أشر اليها واكتب \n*.تحسين*`;
-				if (!/image\/(jpe?g|png)/.test(mime))
-					throw `Mime ${mime} tidak support`;
-				else conn.recolor[m.sender] = true;
-				m.reply(wait);
-				let img = await q.download?.();
-				let error;
-				try {
-					const This = await processing(img, "recolor");
-					conn.sendFile(m.chat, This, "", "تم تحسين الصوره", m);
-				} catch (er) {
-					error = true;
-				} finally {
-					if (error) {
-						m.reply("*فشلت العملية*");
-					}
-					delete conn.recolor[m.chat];
-				}
-			}
-			break;
-		case "hd":
-			{
-				conn.hdr = conn.hdr ? conn.hdr : {};
-				let q = m.quoted ? m.quoted : m;
-				let mime = (q.msg || q).mimetype || q.mediaType || "";
-				if (!mime)
-					throw `ارسل للبوت اي صورة وسوف يقوم برفع جودتها لتصبح HD\n\nارسل الصورة تم أشر اليها واكتب \n*.تحسين*`;
-				if (!/image\/(jpe?g|png)/.test(mime))
-					throw `Mime ${mime} tidak support`;
-				else conn.hdr[m.sender] = true;
-				m.reply(wait);
-				let img = await q.download?.();
-				let error;
-				try {
-					const This = await processing(img, "enhance");
-					conn.sendFile(m.chat, This, "", "تم تحسين الصوره", m);
-				} catch (er) {
-					error = true;
-				} finally {
-					if (error) {
-						m.reply("*فشلت العملية*");
-					}
-					delete conn.hdr[m.sender];
-				}
-			}
-			break;
-	}
-};
-handler.help = ["dehaze","recolor","hd"];
-handler.tags = ["image-edit"];
-handler.command = ["تحسين"];
-export default handler;
+  conn.hdr = conn.hdr ? conn.hdr : {}
+  if (m.sender in conn.hdr)
+    throw "لا تزال هناك عملية لم تكتمل يا صديقي. الرجاء الانتظار حتى تنتهي >//<"
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || q.mediaType || ""
+  if (!mime)
+    throw `أين الصورة التي تريد تحويلها لجودة عالية؟`
+  if (!/image\/(jpe?g|png)/.test(mime))
+    throw `Mime ${mime} غير مدعوم`
+  else conn.hdr[m.sender] = true;
+  m.reply("الرجاء الانتظار قليلاً...")
+  let img = await q.download?.()
+  let error
+  try {
+    const This = await processing(img, "enhance")
+    conn.sendFile(m.chat, This, "", "ها هي الصورة محسنه اشكرني بقى", m)
+  } catch (er) {
+    error = true
+  } finally {
+    if (error) {
+      m.reply("حدث خطأ :(")
+    }
+    delete conn.hdr[m.sender]
+  }
+}
+
+handler.help = ['hd']
+handler.tags = ['tools', 'ai']
+handler.command = /^(تحسين)$/i
+
+handler.register = false
+handler.limit = false
+
+export default handler
 
 async function processing(urlPath, method) {
-	return new Promise(async (resolve, reject) => {
-		let Methods = ["enhance", "recolor", "dehaze"];
-		Methods.includes(method) ? (method = method) : (method = Methods[0]);
-		let buffer,
-			Form = new FormData(),
-			scheme = "https" + "://" + "inferenceengine" + ".vyro" + ".ai/" + method;
-		Form.append("model_version", 1, {
-			"Content-Transfer-Encoding": "binary",
-			contentType: "multipart/form-data; charset=uttf-8",
-		});
-		Form.append("image", Buffer.from(urlPath), {
-			filename: "enhance_image_body.jpg",
-			contentType: "image/jpeg",
-		});
-		Form.submit(
-			{
-				url: scheme,
-				host: "inferenceengine" + ".vyro" + ".ai",
-				path: "/" + method,
-				protocol: "https:",
-				headers: {
-					"User-Agent": "okhttp/4.9.3",
-					Connection: "Keep-Alive",
-					"Accept-Encoding": "gzip",
-				},
-			},
-			function (err, res) {
-				if (err) reject();
-				let data = [];
-				res
-					.on("data", function (chunk, resp) {
-						data.push(chunk);
-					})
-					.on("end", () => {
-						resolve(Buffer.concat(data));
-					});
-				res.on("error", (e) => {
-					reject();
-				});
-			}
-		);
-	});
+  return new Promise(async (resolve, reject) => {
+    let Methods = ["enhance", "recolor", "dehaze"]
+    Methods.includes(method) ? (method = method) : (method = Methods[0]);
+    let buffer,
+      Form = new FormData(),
+      scheme = "https" + "://" + "inferenceengine" + ".vyro" + ".ai/" + method;
+    Form.append("model_version", 1, {
+      "Content-Transfer-Encoding": "binary",
+      contentType: "multipart/form-data; charset=uttf-8",
+    });
+    Form.append("image", Buffer.from(urlPath), {
+      filename: "enhance_image_body.jpg",
+      contentType: "image/jpeg",
+    });
+    Form.submit(
+      {
+        url: scheme,
+        host: "inferenceengine" + ".vyro" + ".ai",
+        path: "/" + method,
+        protocol: "https:",
+        headers: {
+          "User-Agent": "okhttp/4.9.3",
+          Connection: "Keep-Alive",
+          "Accept-Encoding": "gzip",
+        },
+      },
+      function (err, res) {
+        if (err) reject();
+        let data = [];
+        res
+          .on("data", function (chunk, resp) {
+            data.push(chunk);
+          })
+          .on("end", () => {
+            resolve(Buffer.concat(data));
+          });
+        res.on("error", (e) => {
+          reject();
+        });
+      }
+    );
+  });
 }

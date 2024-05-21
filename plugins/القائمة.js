e72fcfee314f907c946792694d9d060120b04b02
Frosty1V <147980724+Frosty1V@users.noreply.github.com>
2024-05-21 14:47:37 +0300

    Update القائمة.js

--- plugins/القائمة.js
+++ plugins/القائمة.js
@@ -1,37 +1,51 @@
-import { createHash } from 'crypto'
-import fetch from 'node-fetch'
 import fs from 'fs'
-import moment from 'moment-timezone'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    conn.relayWAMessage(m.chat, {
+      viewOnceMessage: {
+        message: {
+          interactiveMessage: {
+            header: {
+              title: 'قـائـمـة الـاوامــر'
+            },
+            body: {
+              text: '*▫️  اسم البوت , 𝑅𝒶𝓋𝓑𝓸𝓽*\n*▫️ حط قبل كل امر* *(.)*\n*▫️ اســم الـمطور  𝓕𝓻𝓸𝓼𝓽*'
+            },
+            nativeFlowMessage: {
+              buttons: [
+                {
+                  name: 'single_select',
+                  buttonParamsJson: JSON.stringify({
+                    title: 'دوس هنا ',
+                    sections: [
+                      {
+                        title: 'قوائم البوت',
+                        highlight_label: 'اختار',
+                        rows: [
+                          {
+                            header: 'قسم الانمي',
+                            title: '.الانمي',
+                            description: '',
+                            id: 'te'
+                          },
+                          {
+                            header: 'قسم الاوامر',
+                            title: '.المهام',
+                            description: '',
+                            id: 'te'
+                          }
+                        ]
+                      }
+                    ]
+                  }),
+                  messageParamsJson: ''
+                }
+              ]
+            }
+          }
+        }
+      }
+    })
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-    let pp = './Menu.png'
-    let username = conn.getName(who)
-    let sn = createHash('md5').update(who).digest('hex')
-    let more = String.fromCharCode(8206)
-    let readMore = more.repeat(850)
-    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-
-    const buttons = [
-        { buttonId: `${usedPrefix}ترفيه`, buttonText: { displayText: 'الترفيه' }, type: 1 },
-        { buttonId: `${usedPrefix}مشرفين`, buttonText: { displayText: 'المشرفين' }, type: 1 },
-        { buttonId: `${usedPrefix}اوامر`, buttonText: { displayText: 'الاوامر' }, type: 1 },
-    ]
-    const buttonMessage = {
-        image: { url: pp },
-        caption: `
-*▫️  اسم البوت , 𝑅𝒶𝓋𝓑𝓸𝓽*
-*▫️ حط قبل كل امر* *(.)*
-*▫️ اســم الـمطور  𝓕𝓻𝓸𝓼𝓽*
-╾━━━━━━━━━━━━━━━━━╼
-*يرجى اختيار القسم الذي ترغب في عرضه:*`,
-        footer: 'اختر قسمًا',
-        buttons: buttons,
-        headerType: 4
-    }
-
-    await conn.sendMessage(m.chat, buttonMessage)
 }
 
 handler.help = ['main']

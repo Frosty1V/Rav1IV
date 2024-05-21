b744e9e7b80d4e2decfbfa9d70998a269d091894
Frosty1V <147980724+Frosty1V@users.noreply.github.com>
2024-05-21 14:54:17 +0300

    Update القائمة.js

--- plugins/القائمة.js
+++ plugins/القائمة.js
@@ -18,20 +18,20 @@ let handler = async (m, { conn, usedPrefix, command}) => {
     let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
     let _uptime = process.uptime() * 1000
     let uptime = clockString(_uptime)
-    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-    let pp = './Menu.png'
-    let user = global.db.data.users[who]
-    let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
-    let { min, xp, max } = xpRange(user.level, global.multiplier)
-    let username = conn.getName(who)
-    let math = max - xp
-    let prem = global.prems.includes(who.split`@`[0])
-    let sn = createHash('md5').update(who).digest('hex')
-    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
-    let more = String.fromCharCode(8206)
-    let readMore = more.repeat(850) 
-    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+let pp = './Menu.png'
+let user = global.db.data.users[who]
+let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
+let { min, xp, max } = xpRange(user.level, global.multiplier)
+let username = conn.getName(who)
+let math = max - xp
+let prem = global.prems.includes(who.split`@`[0])
+let sn = createHash('md5').update(who).digest('hex')
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+let more = String.fromCharCode(8206)
+let readMore = more.repeat(850) 
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
     let str = `
 *▫️  اسم البوت , 𝑅𝒶𝓋𝓑𝓸𝓽*
 *▫️ حط قبل كل امر* *(.)*
@@ -90,21 +90,26 @@ handler.tags = ['group']
 handler.command = ['القائمه','قائمه','قائمة','القائمة'] 
 
 export default handler
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
 
-function ucapan() {
-  const time = moment.tz('Egypt').format('HH')
-  let res = "بداية يوم سعيده ☀️"
-  if (time >= 4) {
-    res = "صباح الخير 🌄"
-  }
-  if (time >= 10) {
-    res = "مساء الخير ☀️"
-  }
-  if (time >= 15) {
-    res = "مساء الخير 🌇"
-  }
-  if (time >= 18) {
-    res = "مساء الخير 🌙"
-  }
-  return res
-}
+    function ucapan() {
+      const time = moment.tz('Egypt').format('HH')
+      let res = "بداية يوم سعيده ☀️"
+      if (time >= 4) {
+        res = "صباح الخير 🌄"
+      }
+      if (time >= 10) {
+        res = "مساء الخير ☀️"
+      }
+      if (time >= 15) {
+        res = "مساء الخير 🌇"
+      }
+      if (time >= 18) {
+        res = "مساء الخير 🌙"
+      }
+      return res
+    }
